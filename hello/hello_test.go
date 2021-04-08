564a151c82867804a0b3153a1d75d497f4103eca
takanori-matsushita <t.matsushita@arcjobz.com>
2021-04-08 17:11:44 +0900

    hello.go and test

---
+++ hello/hello_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, world"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("taka", "")
+		want := "Hello, taka"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+}
