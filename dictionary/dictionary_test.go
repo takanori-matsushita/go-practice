27c2093035e3ed0c236ab606e4afb4d156fb310d
takanori-matsushita <t.matsushita@arcjobz.com>
2021-04-23 13:02:34 +0900

    [add]unknown word

--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -4,14 +4,28 @@ import "testing"
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
-	got := dictionary.Search("test")
-	want := "this is just a test"
-	assertStrings(t, got, want)
+	t.Run("know word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		assertError(t, err, errorNotFound)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
 	}
 }
